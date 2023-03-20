12fb379cb9ee6897645cee94ee84f748fd5633e7
Shaik-Kamil <107490051+Shaik-Kamil@users.noreply.github.com>
2023-03-20 20:25:32 +0530

    finished food

--- food.js
+++ food.js
@@ -1,4 +1,27 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    console.log(
+      this.daysToSpoil > 0
+        ? `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+        : `${this.name} has spoiled.`
+    );
+  }
+  aDayPasses() {
+    console.log(this.daysToSpoil = this.daysToSpoil - 1);
+    this.isFresh();
+  }
+}
 
+// const newFood = new Food('Banana', 4, true);
 // Do not edit below this line
+
 module.exports = Food;
