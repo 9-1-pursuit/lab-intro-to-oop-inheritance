9115d3b039d4d6d8c51faead56d6f593250c2013
Kylie-Lang1 <kylielang@pursuit.org>
2023-03-20 11:08:57 -0400

    added prepare method. All tests for food.js passed

--- food.js
+++ food.js
@@ -3,18 +3,22 @@ class Food {
     constructor (name, daysToSpoil, fresh){
         this.name = name
         this.daysToSpoil = daysToSpoil
-        this.fresh = fresh
+        this.fresh = fresh || true
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
     }
     isFresh(){
         if(this.daysToSpoil > 0){
             console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
         } else {
-            console.log(`${this.name} is spoiled.`)
+            console.log(`${this.name} has spoiled.`)
             return this.fresh = false
         }
     }
     aDayPasses(){
         this.daysToSpoil -= 1
+        this.isFresh()
     }
 }
 
@@ -23,10 +27,8 @@ const cake = new Food('cake', 3,true)
 console.log(cake)
 cake.isFresh()
 cake.aDayPasses()
-console.log(cake)
 cake.aDayPasses()
 cake.aDayPasses()
-cake.isFresh()
 console.log(cake)
 
 // Do not edit below this line
