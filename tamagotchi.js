1c93e0e45a7347a88660241e35ba66c9e070b21f
Gillesedgy <edgygilles@pursuit.org>
2023-03-20 19:54:15 -0400

    Lab completed with no bonuses... Passes all Tests

--- tamagotchi.js
+++ tamagotchi.js
@@ -26,9 +26,9 @@ class Tamagotchi {
   }
   medicate() {
     if (this.sick) {
-    this.sick = false;
-    this.full = 9;
-   this.energy -= 3;
+      this.sick = false;
+      this.full = 9;
+      this.energy -= 3;
     } else {
       console.log(`Refusal to take medicine`);
       this.energy -= 1;
@@ -36,8 +36,8 @@ class Tamagotchi {
   }
   play() {
     if (this.sick) {
-        this.mood -=1
-        this.energy -=1
+      this.mood -= 1;
+      this.energy -= 1;
     } else if (this.mood > 9) {
       console.log("I am too happy to play");
       this.energy -= 2;
@@ -50,30 +50,54 @@ class Tamagotchi {
       this.energy -= 1;
       this.full -= 1;
     }
-}
-sleep(){
+  }
+  sleep() {
     this.energy += 4;
-    this.full -=3
-}
-timePasses(){
-   if(this.sick){
-    this.mood-= 3
-    this.energy-=2
-    this.full -=2
-   } 
-   else{
-    this.mood-=2
-    this.full-=1
-    this.energy-=1
-   }
-}
-badGuardian(){
-  if(this.energy <=0 || this.mood <=0 || this.full<=0){
-console.log(`${this.name} has been rehomed`)
-this.rehomed = true
-  }  
-}
-}
-// Do not edit below this line
+    this.full -= 3;
+  }
 
+  timePasses() {
+    if (this.sick) {
+      this.mood -= 3;
+      this.energy -= 2;
+      this.full -= 2;
+    } else {
+      this.mood -= 2;
+      this.full -= 1;
+      this.energy -= 1;
+    }
+  }
+  badGuardian() {
+    if (this.energy <= 0 || this.mood <= 0 || this.full <= 0) {
+      console.log(`${this.name} has been rehomed`);
+      this.rehomed = true;
+    }
+  }
+}
+//NEW
+const newTamagotchi = new Tamagotchi("Nola");
+newTamagotchi.greet();
+
+newTamagotchi.play();
+newTamagotchi.status();
+
+newTamagotchi.sleep();
+newTamagotchi.status();
+
+newTamagotchi.medicate();
+newTamagotchi.status();
+
+newTamagotchi.timePasses();
+newTamagotchi.status();
+/*
+Hello, I'm Nola!
+Energy: 9, Fullness: 8, Mood: 6, Sick: false
+Energy: 8, Fullness: 7, Mood: 8, Sick: false
+Energy: 12, Fullness: 4, Mood: 8, Sick: false
+Refusal to take medicine
+Energy: 11, Fullness: 4, Mood: 8, Sick: false
+Energy: 10, Fullness: 3, Mood: 6, Sick: false
+*/
+
+// Do not edit below this line
 module.exports = Tamagotchi;
