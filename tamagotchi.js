b2daeb444e251eb31ea784f348e0111f16ababa8
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-03-20 13:38:09 -0400

    passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -47,6 +47,54 @@ class Tamagotchi {
       this.energy--;
     }
   }
+
+  //     - play: increases mood by 2, reduces energy and full by 1,
+  //   - will not play if tamagotchi is sick,
+  //   - if asked to play when sick, reduce mood and energy by 1
+  //   - will not play if mood is above 9, reduce energy by 2 and full by 1
+  //   - will not play if energy is less than or equal to 3
+  //     - console log "I am too tired to play" - reduce energy by 1
+
+  play() {
+    if (this.sick) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.energy <= 3) {
+      console.log(`I am too tired to play`);
+      this.energy -= 1;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full--;
+    } else {
+      this.mood += 2;
+      this.energy -= 1;
+      this.full -= 1;
+    }
+  }
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+
+  //     - timePasses:
+  //   - if tamagotchi is not sick - mood decreases by 2, fullness and energy decrease by 1
+  //     -if tamagotchi is sick - mood decreases by 3, fullness and energy decrease by 2
+  timePasses() {
+    if (this.sick) {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    } else {
+      this.mood -= 2;
+      this.full -= 1;
+      this.energy -= 1;
+    }
+  }
+  badGuardian() {
+    if (this.energy <= 0 || this.mood <= 0 || this.full <= 0) {
+      this.rehomed = true;
+    }
+  }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
