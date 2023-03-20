c183a69a11133a44ad167392ced155cc3c01acda
bowersjuan <bowersjuan@gmail.com>
2023-03-20 11:15:39 -0400

    update Tamagotchi class with greet, status and eat methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,6 +8,24 @@ class Tamagotchi {
         this.sick = sick,
         this.rehomed = rehomed
     }
+
+    greet() {
+        console.log(`Hello, I'm ${this.name}`)
+    }
+
+    status() {
+        const sickMessage = this.sick ? 'I am sick' : 'I am not sick'
+        console.log(`My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}\n${sickMessage}`)
+    }
+
+    eat() {
+        this.full += 2;
+        this.energy -= 1;
+
+        if (this.full > 10) {
+            this.sick = true;
+        }
+    }
 }
 
 // Do not edit below this line
