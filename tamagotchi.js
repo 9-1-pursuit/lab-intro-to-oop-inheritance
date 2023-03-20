2de05ec56d0018293005d2fe0e462f20b4aeb56a
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-03-20 12:01:36 -0400

    completed food and created ta magotchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,21 @@
 // Create class below
-class Tamagotchi {}
+class Tamagotchi {
+  consructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  // - greet: Tamagotchi console logs `Hello, I'm <name>!`
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
