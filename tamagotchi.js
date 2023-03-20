d578c7c138714ea70245172c25953197d30365a5
Gillesedgy <edgygilles@pursuit.org>
2023-03-20 11:59:35 -0400

    created Tamagatchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,14 @@
 // Create class below
 class Tamagotchi{
-    
+  consteructor(name){
+    this.name = name;
+    this.energy = 9
+    this.full_energy = 8
+    this.mood= 6
+    this.sick = false
+    this.rehomed =false
+  }
+
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
