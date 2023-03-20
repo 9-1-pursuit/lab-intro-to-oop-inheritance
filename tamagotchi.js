cd27c2978772ba4731b047c4d97e41e2fa4854c2
D-Stukes <dstukes888@gmail.com>
2023-03-20 13:39:14 -0400

    completed and passed remainder of tests

--- tamagotchi.js
+++ tamagotchi.js
@@ -30,10 +30,55 @@ class Tamagotchi {
             this.full = 9
             this.energy -=3
             this.sick = false
-    } else {
-        console.log ("refusal to take medicine")
+        } else {
+            console.log ("refusal to take medicine")
+            this.energy -= 1
+        }
+   }
+    play(){
+        if(this.sick) {
+        console.log( "I am too sick to play")
+        this.mood -=1
+        this.energy -=1
+        } else if(this.mood > 9){
+            this.sick = false
+            console.log("will not play")
+            this.energy -= 2
+            this.full -= 1
+        } else if(this.energy <= 3){
+            console.log("I am too tired to play")
+            this.energy -= 1
+        } else {
+
+        this.mood +=2
+        this.energy -=1
+        this.full -=1
+       }
     }
-  }
-}
+
+    sleep(){
+        this.energy +=4
+        this.full -=3   
+    }
+
+    timePasses(){
+        if(!this.sick){
+            this.mood -=2
+            this.full -=1
+            this.energy -=1
+        } else {
+            this.mood -=3
+            this.full -=2
+            this.energy -=2
+        }
+    }
+    badGuardian(){
+        console.log("The Tamagotchi has been rehomed")
+        this.energy <=0 ? this.rehomed = true : false
+        this.full <=0 ? this.rehomed = true : false
+    }
+
+    }
+
 // Do not edit below this line
 module.exports = Tamagotchi;
