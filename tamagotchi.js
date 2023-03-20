10d18f5a371cc3487539331226647286a2acb05d
Salinamalek <salinamalek@pursuit.org>
2023-03-20 11:29:54 -0400

    greet and status methods added

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,6 +8,16 @@ class Tamagotchi {
     this.sick = false;
     this.rehomed = false;
   }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
+
+  status() {
+    console.log(
+      `Energy: ${this.energy}, Fullness: ${this.full}, Mood: ${this.mood}, Sick: ${this.sick}`
+    );
+  }
 }
 
 // Do not edit below this line
