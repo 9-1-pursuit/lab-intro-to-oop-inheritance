4f57b5f76209c729e0689061db031030cf4d269a
MarsiaRochelin <marsrochelin@pursuit.org>
2023-03-20 19:35:39 -0400

    constructed Tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,25 @@
 // Create class below
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    (this.name = name),
+      (this.energy = energy),
+      (this.full = full),
+      (this.mood = mood),
+      (this.sick = sick),
+      (this.rehomed = rehomed);
+  }
+}
+const fluffy = new Tamagotchi("Fluffy");
 
+// fluffy.eat();
+// console.log(fluffy.medicate());
+console.log(fluffy.status());
 // Do not edit below this line
 module.exports = Tamagotchi;
