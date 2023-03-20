7d961bec97a601db99e6792464dd04c777d6b11a
Gillesedgy <edgygilles@pursuit.org>
2023-03-20 11:00:13 -0400

    created food class

--- food.js
+++ food.js
@@ -1,4 +1,20 @@
 // Create class below
-
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil; // value is not known yet =>
+    this.fresh = fresh || true;
+  }
+  prepared(name) {
+    console.log(`${name} is being prepared`);
+  }
+  isFresh(daysToSpoil, name) {
+    if (daysToSpoil) {
+      console.log(`There are ${daysToSpoil} days left before ${name} spoils`);
+    } else {
+      `${name} has spoiled.`;
+    }
+  }
+}
 // Do not edit below this line
 module.exports = Food;
