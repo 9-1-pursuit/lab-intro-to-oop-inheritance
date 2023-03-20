6967a6fefabe708d93c0f69c5dbdd09e1256c54c
DestinyJoyner <destinyjoyner@pursuit.org>
2023-03-20 15:38:23 -0400

    food fight game complete

--- bonus.js
+++ bonus.js
@@ -4,23 +4,52 @@ const { donut, pizza } = require("./bonusData.js")
 class BadFood extends Food {
     constructor(name, daysToSpoil, fresh=true, weapons){
         super(name, daysToSpoil, fresh)
-        // no weapons param
-        // this.weapons = [donut, pizza]
-        // with weapons param
         this.weapons = weapons
     }
     prepare(){
         const msg1 = `I am ${this.name} and my calories are too high to count!`
         const msg2 = `I am ${this.name} and you are just a passing trend!`
         const arr = [msg1, msg2]
-        const index = Math.floor(Math.random() *2)
+        const index = Math.floor(Math.random() * arr.length)
         console.log(arr[index])  
     }
-    fight(classVal){
+    fight(rival){
+        // while loop or recursion -> fight to the death lol
+        // let myAttack = this.weapons[Math.floor(Math.random() * this.weapons.length)].hitPoints
+        // let rivalAttack = rival.weapons[Math.floor(Math.random() * rival.weapons.length)].hitPoints
+        // console.log(myAttack,rivalAttack)
 
+        let myHealth = this.daysToSpoil
+        let rivalHealth = rival.daysToSpoil
+
+        while(myHealth > 0 || rivalHealth > 0){
+            // random select/ retrieve attack points/name
+            const myIndex = Math.floor(Math.random() * this.weapons.length)
+            let myAttackPoints = this.weapons[myIndex].hitPoints
+            let myAttackName = this.weapons[myIndex].name
+            
+            const rivalIndex = Math.floor(Math.random() * rival.weapons.length)
+            let rivalAttackPoints = rival.weapons[rivalIndex].hitPoints
+            let rivalAttackName = rival.weapons[rivalIndex].name
+            
+            rivalHealth -= myAttackPoints
+            
+            console.log(`${this.name} uses ${myAttackName}!! \n${rival.name} is down ${myAttackPoints} \n${rival.name} ${rivalHealth} ${this.name} ${myHealth}`)
+            
+            if(rivalHealth < 0) break;
+
+            myHealth -= rivalAttackPoints
+            
+            console.log(`${rival.name} uses ${rivalAttackName}!!! \n${this.name} is down ${rivalAttackPoints} \n${this.name} ${myHealth} ${rival.name} ${rivalHealth}`)
+           
+            if(myHealth < 0) break;
+        }
+        return rivalHealth <= 0 ? `${this.name} is the winner!` : `Better luck next time, ${rival.name} was better.`
+        
     }
     
 }
 
-const testInherit = new BadFood("testFood", 2, false, donut)
-console.log(testInherit.weapons)
+const testDonut = new BadFood("Donut", 22, false, donut)
+const testPizza = new BadFood("Pizza", 18, true, pizza)
+console.log(testDonut.fight(testPizza))
