4f75879ca2d9c10177fb5ea5e94df0dcadbd4e2c
Kylie-Lang1 <kylielang@pursuit.org>
2023-03-20 12:29:33 -0400

    added console.logs for funcs and test cases

--- tamagotchi.js
+++ tamagotchi.js
@@ -9,20 +9,17 @@ class Tamagotchi {
         this.rehomed = rehomed || false
     }
     greet(){
-        console.log(`Hello, I'm ${this.name}`)
+        console.log(`Hello, I'm ${this.name}!`)
     }
     status(){
-        console.log(`
-        My mood is: ${this.mood}\n
-        I am this full: ${full}\n
-        My energy is: ${energy}\n
-        ${this.sick ? 'I am sick' : 'i am not sick'}`)
+        console.log(`My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}\n${this.sick ? 'I am sick' : 'I am not sick'}`)
     }
     eat(){
         this.full += 2
         this.energy -= 1
         if (this.full > 10){
             this.sick = true
+            console.log(`${this.name} ate too much`)
         } 
         
     }
@@ -31,6 +28,7 @@ class Tamagotchi {
             this.full = 9
             this.energy -= 3
             this.sick = false
+            console.log(`${this.name} feels better`)
         } else {
             this.energy -= 1
             console.log(`${this.name} refuses the medicine`)
@@ -39,10 +37,12 @@ class Tamagotchi {
     play(){
         if (this.sick){
             this.mood -= 1
-            this.energy -= 1  
+            this.energy -= 1
+            console.log(`I am too sick to play`)  
         } else if (this.mood > 9){
             this.energy -= 2
             this.full -= 1
+            console.log(`I don't want to play`)
         } else if (this.energy <= 3){
             this.energy -= 1
             console.log(`I am too tired to play`)
@@ -50,17 +50,20 @@ class Tamagotchi {
             this.mood += 2
             this.full -= 1
             this.energy -= 1
+            console.log(`Let's play`)
         }
     }
     sleep(){
         this.energy += 4
         this.full -= 3
+        console.log(`${this.name} is sleeping`)
     }
     timePasses(){
+        console.log(`Some time has passed`)
         return this.sick 
             ? ( this.mood -= 3, 
                 this.full -= 2,
-                this.energy -=2
+                this.energy -= 2
                 ) : (
                     this.mood -= 2,
                     this.full -= 1,
@@ -75,5 +78,21 @@ class Tamagotchi {
         }
     }
 }
+
+// Test cases
+const lily = new Tamagotchi ('Lily')
+console.log(lily) 
+lily.greet()
+lily.status()
+lily.eat()
+lily.eat()
+lily.eat()
+console.log(lily)
+lily.play()
+lily.medicate()
+lily.sleep()
+lily.timePasses()
+lily.badGuardian()
+
 // Do not edit below this line
 module.exports = Tamagotchi;
