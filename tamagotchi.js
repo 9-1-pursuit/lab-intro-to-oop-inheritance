7df0c21a6717891ff7aa5bca9ff7dc5374ee8a51
PESolut <jahaadpetty@pursuit.org>
2023-03-24 14:01:49 -0400

    created food and tomagachi class, tomagachi class has eat and status method

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,31 @@
 // Create class below
+class Tamagotchi {
+    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
+        this.name = name
+        this.energy = energy
+        this.full = full
+        this.mood = mood
+        this.sick = sick
+        this.rehomed = rehomed
+    }
+    greet(){
+        console.log(`Hello, I'm ${this.name}`)
+    }
+    status(){
+        console.log('*********************\n')
+        console.log(`My mood is: ${this.mood}`)
+        console.log(`I am this full: ${this.full}`)
+        console.log(`My energy is: ${this.energy}`)
+        console.log(this.isSick ? `I am sick` : 'I am not sick')
+    }
+    eat(){
+        this.full = this.full + 2
+        this.energy--
+        if(this.full > 10){
+            this.sick = true
+        }
+    }
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
