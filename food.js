53f18742a13a8ff61fbb0ec27542e1c01222fe4a
chrisking718 <chrisking@pursuit.org>
2023-03-27 06:59:09 -0400

    43 test passed

--- food.js
+++ food.js
@@ -1,4 +1,36 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil) {
+      this.name = name;
+      this.daysToSpoil = daysToSpoil;
+      this.fresh = true;
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.daysToSpoil>0){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        }else{
+            this.fresh = false;
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil--
+        this.isFresh()
+    }
+
+  }
+
+  const chicken = new Food("chicken","3")
+  chicken.aDayPasses()
+  console.log(chicken)
+  chicken.aDayPasses()
+  console.log(chicken)
+  chicken.aDayPasses()
+  console.log(chicken)
+  
 
 // Do not edit below this line
 module.exports = Food;
