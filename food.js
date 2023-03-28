c95355e70731221903eb6604e0dff5d7ed2440c4
Lennie Nurse <lnursej@gmail.com>
2023-03-28 17:53:32 -0400

    updated lab got 41/42 correct

--- food.js
+++ food.js
@@ -1,4 +1,26 @@
 // Create class below
-
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = "Banana"
+    this.daysToSpoil = 3
+    this.fresh = true
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`)
+  }
+  isFresh() {
+    if (this.daysToSpoil > 0) {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      )
+    } else {
+      console.log(`eeewww what an old ${this.name} has spoiled.`)
+    }
+  }
+  aDayPasses() {
+    this.daysToSpoil--
+    this.isFresh()
+  }
+}
 // Do not edit below this line
-module.exports = Food;
+module.exports = Food
