31c4b6bed809b9b8a90b63d1c836b012bbb46ffc
melanieinpursuit <melaniegonzalez@pursuit.org>
2023-04-14 10:44:45 -0400

    all but one passing

--- food.js
+++ food.js
@@ -1,4 +1,35 @@
 // Create class below
+class Food {
+    constructor() {
+     this.name = "Banana"
+     this.daysToSpoil = 3
+     this.fresh = true    
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh() {
+        if (this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        }
+        else {
+            console.log(`eeewww what an old ${this.name} has spoiled.`)
+        }    
+}
+    aDayPasses() {
+        this.daysToSpoil--
+        this.isFresh()
+    }
+}
 
-// Do not edit below this line
+// create a new Food 
+
+const iceCream = new Food ("iceCream", 6)
+iceCream.prepare()
+iceCream.isFresh()
+iceCream.aDayPasses()
+
+const test = new Food ()
+console.log(test)
+
 module.exports = Food;
